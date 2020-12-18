401cb89b396ff9abc082e157f5d5952689a94be9
ankitdashtechinc <ankitp.dashtechinc@gmail.com>
2020-12-18 08:20:36 -0500

    Ankit - Maven Project

---
+++ src/main/java/Login/Login.java
@@ -0,0 +1,10 @@
+package Login;
+
+public class Login {
+	
+	public void loginintoapp()
+	{
+		System.out.println("I am logged into the Application");
+	}
+
+}
---
+++ src/test/java/LoginTest/LoginTest.java
@@ -0,0 +1,17 @@
+package LoginTest;
+
+
+import org.testng.annotations.Test;
+
+import Login.Login;
+
+public class LoginTest extends Login{
+	
+	@Test
+	public boolean testlogin()
+	{
+		boolean result = true;
+		return result;
+	}
+
+}
